baf052fb4af08b27c3939394fef0d72ee3757ff7
Edson Alves <edson.f.alves09@gmail.com>
2020-10-19 22:02:38 -0300

    Add Admin navbar

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -4,15 +4,17 @@ import { ProductsResponse } from 'core/types/Product';
 import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
+import Pagination from 'core/components/Pagination';
 import './styles.scss';
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
 
@@ -22,7 +24,7 @@ const Catalog = () => {
         .finally(() => {
             setIsLoading(false);
         })
-    }, []);
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -33,12 +35,18 @@ const Catalog = () => {
                 {isLoading ? <ProductCardLoader /> : (
                     productsResponse?.content.map(product => (
                         <Link to={`/products/${product.id}`} key={product.id}>
-                            <ProductCard product={product}/>
+                            <ProductCard product={product} />
                         </Link>
                     ))
                 )}
-                {}
             </div>
+            {productsResponse && (
+                <Pagination 
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />
+            )}
         </div>
     );
 };
